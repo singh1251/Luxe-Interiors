ed5001508bb4ac06ce8286b77bf6ccb7cf964e37
Ashutosh Singh <singhashutosh1251@gmail.com>
2024-07-20 18:18:30 +0530

    Updated the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 import { Resources } from "../assets/Resources";
 
@@ -32,15 +33,16 @@ export default function Footer() {
       <div className="w-full my-5 flex justify-center">
         <div className="bg-gray-200 h-px flex-1 sm:self-stretch"></div>
       </div>
+
       <section className="w-[90%] flex flex-wrap tablet:flex-nowrap tablet:justify-between">
         <div className="w-full tablet:w-[25%]">
           <p className="text-2xl font-bold">LuxeInteriors</p>
-          <p className="text-gray-500 my-4 text-base">
+          <p className="text-gray-500 my-4">
             Worldwide furniture store since 2020. We sell over 1000+ branded
             products on our website
           </p>
           <ul>
-            <li className="text-gray-500 my-4 text-base">
+            <li className="text-gray-500 my-4">
               <img
                 src={Resources.locationIcon}
                 alt="icon"
@@ -48,7 +50,7 @@ export default function Footer() {
               />{" "}
               New York, USA
             </li>
-            <li className="text-gray-500 my-4 text-base">
+            <li className="text-gray-500 my-4">
               <img
                 src={Resources.phoneIcon}
                 alt="icon"
@@ -56,7 +58,7 @@ export default function Footer() {
               />{" "}
               +1 408 XXX XXXX
             </li>
-            <li className="text-gray-500 my-4 text-base">
+            <li className="text-gray-500 my-4">
               <img
                 src={Resources.globeIcon}
                 alt="icon"
@@ -71,13 +73,13 @@ export default function Footer() {
           <div className="w-full my-4 tablet:w-[25%]">
             <p className="font-bold text-lg">Sitemap</p>
             <ul>
-              <li className="text-gray-500 my-4 text-base">
+              <li className="text-gray-500 my-4">
                 <a href="#">Products</a>
               </li>
-              <li className="text-gray-500 my-4 text-base">
+              <li className="text-gray-500 my-4">
                 <a href="#">Services</a>
               </li>
-              <li className="text-gray-500 my-4 text-base">
+              <li className="text-gray-500 my-4">
                 <a href="#">Articles</a>
               </li>
               <li className="text-gray-500 mt-4 mb-2">
@@ -87,19 +89,25 @@ export default function Footer() {
           </div>
 
           <div className="w-full my-4 tablet:w-[25%]">
-            <p className="font-bold text-lg">Category</p>
+            <p className="font-bold text-lg">Categories</p>
             <ul>
-              <li className="text-gray-500 my-4 text-base">
-                <a href="#">Living Room</a>
+              <li className="text-gray-500 my-4">
+                <Link to="/chairCollection">Chairs</Link>
               </li>
-              <li className="text-gray-500 my-4 text-base">
-                <a href="#">Bed Room</a>
+              <li className="text-gray-500 my-4">
+                <Link to="/loveseatCollection">Loveseats</Link>
               </li>
-              <li className="text-gray-500 my-4 text-base">
-                <a href="#">Kitchen</a>
+              <li className="text-gray-500 my-4">
+                <Link to="/daybedCollection">Daybeds</Link>
               </li>
-              <li className="text-gray-500 mt-4 mb-2 text-base">
-                <a href="#">Bath Room</a>
+              <li className="text-gray-500 mt-4 mb-2">
+                <Link to="/sectionalCollection">Sectionals</Link>
+              </li>
+              <li className="text-gray-500 mt-4 mb-2">
+                <Link to="/officeSofaCollection">Office Sofas</Link>
+              </li>
+              <li className="text-gray-500 mt-4 mb-2">
+                <Link to="sleeperSofaCollection">Sleeper Sofas</Link>
               </li>
             </ul>
           </div>
@@ -107,19 +115,19 @@ export default function Footer() {
           <div className="w-full my-4 tablet:w-[25%]">
             <p className="font-bold text-lg">Our Company</p>
             <ul>
-              <li className="text-gray-500 my-4 text-base">
+              <li className="text-gray-500 my-4">
                 <a href="#">About Us</a>
               </li>
-              <li className="text-gray-500 my-4 text-base">
+              <li className="text-gray-500 my-4">
                 <a href="#">Vaccancies</a>
               </li>
-              <li className="text-gray-500 my-4 text-base">
+              <li className="text-gray-500 my-4">
                 <a href="#">Privacy Policy</a>
               </li>
-              <li className="text-gray-500 my-4 text-base">
+              <li className="text-gray-500 my-4">
                 <a href="#">Return Policy</a>
               </li>
-              <li className="text-gray-500 mt-4 mb-2 text-base">
+              <li className="text-gray-500 mt-4 mb-2">
                 <a href="#">Refund Policy</a>
               </li>
             </ul>
@@ -150,9 +158,11 @@ export default function Footer() {
           </form>
         </div>
       </section>
+
       <div className=" my-5 flex justify-center w-full">
         <div className="bg-gray-200 h-px flex-1 sm:self-stretch"></div>
       </div>
+
       <p className="text-sm text-gray-500 w-[90%]">
         © 2023 by Luxe Interiors all rights reserved.
       </p>
