33ed640a1efa863c76863b1fd42d87aef4448239
Ashutosh Singh <singhashutosh1251@gmail.com>
2024-07-21 11:10:58 +0530

    Improved the rating component

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -2,9 +2,9 @@ import { Resources } from "../assets/Resources";
 
 export default function Rating({ rating = "4.5", outOf = "545" }) {
   return (
-    <div className="flex">
+    <div className="mt-[0.20rem] sm:mt-[0.13rem] flex">
       <img src={Resources.starIcon} alt="icon" className="mx-1" />{" "}
-      <p>
+      <p className="text-sm sm:text-base">
         <span>{rating}</span> <span className=" text-gray-500">({outOf})</span>
       </p>
     </div>
